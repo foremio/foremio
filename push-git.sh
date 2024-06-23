ff26c783f20f13e40db2150e4b7ae5abdf95fb27
喵~ <157527925+foremio@users.noreply.github.com>
2024-06-23 23:04:38 +0800

    Update

--- push-git.sh
+++ push-git.sh
@@ -3,6 +3,6 @@ git init
 git rm -r --cached
 git add .
 git commit -m "$(date +' %j 天 %c')"
-git remote add origin git@github.com:GinkoCai/GinkoCai.git
+git remote add origin git@github.com:foremio/foremio.git
 git branch main
 git push -f -u origin main
